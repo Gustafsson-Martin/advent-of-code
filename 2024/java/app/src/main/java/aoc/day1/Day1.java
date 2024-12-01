bda29790e7d229240aa08a08c94e1ea3458e0b04
Martin Gustafsson <martin.gustafsson.work@gmail.com>
2024-12-01 17:38:32 +0100

    Solve day1 second challenge with some more bad code.

--- 2024/java/app/src/main/java/aoc/day1/Day1.java
+++ 2024/java/app/src/main/java/aoc/day1/Day1.java
@@ -4,14 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 public class Day1 {
     public static void main(String[] args) {
-        int distance = execute();
-        System.out.println("Distance: " + distance);
+        List<List<Integer>> input = readInput();
+        List<Integer> left = input.get(0);
+        List<Integer> right = input.get(1);
+
+        System.out.println("Distance: " + orderedListDistance(left, right));
+        System.out.println("Similarity Score: " + totalSimilarityScore(left, right));
     }
-    public static int execute() {
+
+    public static List<List<Integer>> readInput() {
         List<Integer> left = new java.util.ArrayList<>(List.of());
         List<Integer> right = new java.util.ArrayList<>(List.of());
 
@@ -33,9 +39,8 @@ public class Day1 {
             throw new RuntimeException(e);
         }
 
-        return orderedListDistance(left, right);
+        return List.of(left, right);
     }
-
     public static int orderedListDistance(List<Integer> left, List<Integer> right) {
         left.sort(Integer::compareTo);
         right.sort(Integer::compareTo);
@@ -45,5 +50,19 @@ public class Day1 {
             distance += Math.abs(left.get(i) - right.get(i));
         }
         return distance;
+    }
+
+    public static int totalSimilarityScore(List<Integer> left, List<Integer> right) {
+        left.sort(Integer::compareTo);
+        HashMap<Integer, Integer> rightValueCount = new HashMap<>();
+        for (Integer val : right) {
+            rightValueCount.put(val, rightValueCount.getOrDefault(val, 0) + 1);
+        }
+
+        int score = 0;
+        for (Integer val : left) {
+            score += val * rightValueCount.getOrDefault(val, 0);
+        }
+        return score;
     }
 }
