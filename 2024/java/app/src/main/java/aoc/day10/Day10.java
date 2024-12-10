2417c47916af37523d27912e75898f548efce0e7
Martin Gustafsson <martin.gustafsson.work@gmail.com>
2024-12-10 18:51:23 +0100

    Solve day 10 with terrible code.

--- 2024/java/app/src/main/java/aoc/day10/Day10.java
+++ 2024/java/app/src/main/java/aoc/day10/Day10.java
@@ -2,8 +2,7 @@ package aoc.day10;
 
 import aoc.Util;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Day10 {
     public static void main(String[] args) {
@@ -13,13 +12,27 @@ public class Day10 {
         System.out.println("Part 2: " + part2(input));
     }
 
-    private static final class Cord {
+
+    private static class Cord {
         public int x;
         public int y;
 
-        public Cord(int x, int y) {
+        Cord(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cord cord = (Cord) o;
+            return x == cord.x && y == cord.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 
@@ -42,15 +55,48 @@ public class Day10 {
 
         int sum = 0;
         for (Cord start : trailStarts) {
-            sum += findTrail(grid, start, 0);
+            sum += trailPeaksAccessibleFrom(grid, start);
         }
 
         return sum;
     }
 
-    private static int trailHeadScore(int[][] grid, Cord trailHead) {
+    private static int trailPeaksAccessibleFrom(int[][] grid, Cord start) {
+        Set<Cord> trailPeaksAccessible = new HashSet<>();
 
+        trailPeaksAccessibleFromRecursive(grid, start, trailPeaksAccessible);
+
+        return trailPeaksAccessible.size();
     }
+
+    private static Set<Cord> trailPeaksAccessibleFromRecursive(int[][] grid, Cord current, Set<Cord> trailPeaksAccessible) {
+        if (grid[current.y][current.x] == 9) {
+            trailPeaksAccessible.add(current);
+            return trailPeaksAccessible;
+        }
+
+        List<List<Integer>> directions = new java.util.ArrayList<>(List.of(
+            List.of(0, 1),
+            List.of(0, -1),
+            List.of(1, 0),
+            List.of(-1, 0)
+        ));
+        for (List<Integer> direction : directions) {
+            int x = current.x + direction.get(0);
+            int y = current.y + direction.get(1);
+            if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length) {
+                continue;
+            }
+
+            Cord newCord = new Cord(x, y);
+            if (grid[y][x] == grid[current.y][current.x] + 1) {
+                trailPeaksAccessibleFromRecursive(grid, newCord, trailPeaksAccessible);
+            }
+        }
+
+        return trailPeaksAccessible;
+    }
+
 
     private static int findTrail(int[][] grid, Cord current, int trailLength) {
         if (grid[current.y][current.x] == 9) {
@@ -81,6 +127,53 @@ public class Day10 {
     }
 
     public static long part2(List<String> input) {
-        return 0;
+        final int width = input.getFirst().length();
+        final int height = input.size();
+
+        int[][] grid = new int[height][width];
+        List<Cord> trailStarts = new java.util.ArrayList<>(List.of());
+        for (int y = 0; y < height; y++) {
+            String line = input.get(y);
+            for (int x = 0; x < width; x++) {
+                int value = Character.getNumericValue(line.charAt(x));
+                grid[y][x] = value;
+                if (value == 0) {
+                    trailStarts.add(new Cord(x, y));
+                }
+            }
+        }
+
+        int sum = 0;
+        for (Cord start : trailStarts) {
+            sum += pathsToPeakFrom(grid, start);
+        }
+
+        return sum;
+    }
+
+    private static int pathsToPeakFrom(int[][] grid, Cord current) {
+        if (grid[current.y][current.x] == 9) {
+            return 1;
+        }
+
+        int sum = 0;
+        List<List<Integer>> directions = new java.util.ArrayList<>(List.of(
+                List.of(0, 1),
+                List.of(0, -1),
+                List.of(1, 0),
+                List.of(-1, 0)
+        ));
+        for (List<Integer> direction : directions) {
+            int x = current.x + direction.get(0);
+            int y = current.y + direction.get(1);
+            if (x < 0 || x >= grid[0].length || y < 0 || y >= grid.length) {
+                continue;
+            }
+
+            if (grid[y][x] == grid[current.y][current.x] + 1) {
+                sum += pathsToPeakFrom(grid, new Cord(x, y));
+            }
+        }
+        return sum;
     }
 }
--- 2024/java/app/src/test/java/aoc/day10/Day10Test.java
+++ 2024/java/app/src/test/java/aoc/day10/Day10Test.java
@@ -9,10 +9,14 @@ class Day10Test {
     @Test
     void part1() {
         var input = """
-0123
-1234
-8765
-9876
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
                 """.lines().toList();
 
         assertEquals(36, Day10.part1(input));
@@ -20,5 +24,17 @@ class Day10Test {
 
     @Test
     void part2() {
+        var input = """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
+                """.lines().toList();
+
+        assertEquals(81, Day10.part2(input));
     }
 }
